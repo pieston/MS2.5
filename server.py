a5466f7ab33045a4fbd87995b839a0801c327e5e
pieston <pedazoman12@gmail.com>
2024-09-03 21:27:19 +0000

    Mundo guardado 2024-09-03

--- server.py
+++ server.py
@@ -6,7 +6,7 @@
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
-# Código          Lugar
+# Código          Lugar1
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapstore)
 # au		      Australia (Sydney)
